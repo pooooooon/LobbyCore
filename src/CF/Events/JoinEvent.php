931640f50a5fbe5592156d3499a5634e6467cb51
SrClau <69334014+SrClau@users.noreply.github.com>
2020-10-05 11:42:26 -0300

    Update JoinEvent.php

--- src/CF/Events/JoinEvent.php
+++ src/CF/Events/JoinEvent.php
@@ -1,1 +1,23 @@
+<?php
 
+namespace CF\Events;
+
+use pocketmine\event\Listener as LT;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerRespawnEvent;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\utils\TextFormat as Text;
+/**
+* @var Just in case
+**/
+use pocketmine\item\Item;
+/**
+* @var Needed for teleportation of the selected world 
+**/
+use pocketmine\level\{Position, Level};
+
+class EventJoin implements LT {
+
+
+}
+?>
