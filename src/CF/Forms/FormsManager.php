2cf41320b8193269490cec0fb8c52d112f59e810
SrClau <69334014+SrClau@users.noreply.github.com>
2020-10-05 12:58:01 -0300

    Update FormsManager.php

--- src/CF/Forms/FormsManager.php
+++ src/CF/Forms/FormsManager.php
@@ -1,1 +1,30 @@
+<?php
 
+namespace CF\Forms;
+
+use pocketmine\utils\{Config, TextFormat as Text};
+use pocketmine\{Player, Server};
+use CF\Forms\{CustomForm, SimpleForm, ModalForm};
+
+class FormsManager {
+  
+public static function getCosmetic(Player $player){
+  
+  
+  
+  
+  }
+  
+public static function getGames(Player $player){
+  
+  
+  }
+
+public static function getInfo(Player $player){
+  
+  }
+  
+}
+
+
+  
