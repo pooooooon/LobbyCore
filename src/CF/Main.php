e7042eaaf61af628b527bfe3c93f1e2bc2122663
SrClau <69334014+SrClau@users.noreply.github.com>
2020-10-04 22:40:23 -0300

    Update Main.php

--- src/CF/Main.php
+++ src/CF/Main.php
@@ -1,1 +1,24 @@
 
+<?php
+
+namespace CF;
+
+use pocketmine\plugin\PluginBase as PB;
+use pocketmine\event\Listener as LT;
+
+class Main extends PB implements LT {
+
+public static $instancia;
+
+public function onLoad(){
+$this->getLogget()->info("Loading LobbyCore..");
+}
+
+public function onEnable(){
+$this->getLogger()->notice("Plugin LobbyCore Enabled");
+$this->getLogger()->info("Created by SrClauYT + iFail90");
+}
+
+public static function getCore(): Main {
+return self::$instancia;
+}
