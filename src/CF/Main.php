2f5784cc4daf32fb7830956abb2c9ef915b6a051
SrClau <69334014+SrClau@users.noreply.github.com>
2020-10-05 14:18:34 -0300

    Update Main.php

--- src/CF/Main.php
+++ src/CF/Main.php
@@ -6,19 +6,22 @@ use pocketmine\plugin\PluginBase as PB;
 use pocketmine\event\Listener as LT;
 use pocketmine\{Player, Server};
 use CF\ScoreAPI;
-  
+use CF\Commands\{Hub, Core};
 class Main extends PB implements LT {
 
 public static $instancia;
 
 public function onLoad(){
+self::$instancia = $this;
 $this->getLogger()->info("Loading LobbyCore..");
 }
 
 public function onEnable(){
+$this->getServer()->getCommandMap()->register("hub", new Hub($this));
+$this->getServer()->getCommandMap()->register("core", new Core($this));
 $this->getLogger()->notice("Plugin LobbyCore Enabled");
 $this->getLogger()->info("Created by SrClauYT + iFail90");
- $this->scoreapi = new ScoreAPI($this);
+$this->scoreapi = new ScoreAPI($this);
 }
 
 public static function getCore(): Main {
