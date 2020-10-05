e152494ca4b6cb2679c67dade80a6b500d0f396a
SrClau <69334014+SrClau@users.noreply.github.com>
2020-10-04 22:54:32 -0300

    Update Items.php

--- src/CF/Items.php
+++ src/CF/Items.php
@@ -1,1 +1,31 @@
+<?php
 
+namespace CF;
+
+use pocketmine\utils\TextFormat as Text;
+use pocketmine\item\Item;
+use pocketmine\scheduler\Task;
+use pocketmine\utils\Config;
+use pocketmine\level\Position;
+use pocketmine\{Player, Server};
+use pocketmine\network\mcpe\protocol\LevelEventPacket;
+use pocketmine\network\mcpe\protocol\LevelSoundEventPacket;
+use CF\Main;
+
+class Items extends Task {
+  
+private $player;
+
+public function __construct(Player $player){
+$this->player = $player;
+	}
+
+public function onRun(int $currentTick){
+$this->setItems($this->player);
+                     }
+
+public function setItems(Player $player){
+$inventory = $player->getInventory();
+$inventory->setItem(5, Item::get(345, 0, 1)->setCustomName("§1§kA§r§bTravel§1§kA");
+}
+?>
