dcde88acd520dce51154558ab4d6686f76542187
SrClau <69334014+SrClau@users.noreply.github.com>
2020-10-05 13:15:27 -0300

    Update FormsManager.php

--- src/CF/Forms/FormsManager.php
+++ src/CF/Forms/FormsManager.php
@@ -9,11 +9,23 @@ use CF\Forms\{CustomForm, SimpleForm, ModalForm};
 class FormsManager {
   
 public static function getCosmetic(Player $player){
-  
-  
-  
-  
+$form = new SimpleForm(function (Player $player, int $data = null){
+if($data === null){
+return true;
   }
+switch ($data){
+case 0:
+    
+break;
+}
+});
+$form->setTitle("§bCosmetics UI");
+$form->setContent("§aChoose a cosmetic");
+$form->addButton("§eFly"."\n"."§7Click here");
+$form->addButton("§eSize"."\n"."§7Click here");
+$form->addButton("§eNickName"."\n"."§7Click here");
+$form->sendToPlayer($player);
+}
   
 public static function getGames(Player $player){
   
