3717508e7869bef24e49751901935e3365d3d98f
iFail90 <71684482+iFail90@users.noreply.github.com>
2020-10-05 11:00:34 -0500

    Update Items.php

--- src/CF/Items.php
+++ src/CF/Items.php
@@ -28,7 +28,7 @@ public function setItems(Player $player){
 $inventory = $player->getInventory();
 $inventory->setItem(5, Item::get(345, 0, 1)->setCustomName("§1§kA§r§bTravel§1§kA"));
 $inventory->setItem(0, Item::get(234, 0, 1)->setCustomName("§b§kB§r§aCosmetics§b§kB"));
-$invenyory->setItem(8, Item::get(450, 0, 1)->setCustomName("§e§kC§e§fInformation§e§kC"));
+$inventory->setItem(8, Item::get(450, 0, 1)->setCustomName("§e§kC§e§fInformation§e§kC"));
 }
 }
 ?>
