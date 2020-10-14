a6736b6442d3a2b6076e17af6acb1c272fd0e36c
SrClauYT <69334014+SrClau@users.noreply.github.com>
2020-10-14 00:34:59 -0300

    JoinEvent.php

--- src/CF/Events/JoinEvent.php
+++ src/CF/Events/JoinEvent.php
@@ -8,7 +8,7 @@ use pocketmine\event\player\PlayerRespawnEvent;
 use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\utils\TextFormat as Text;
 /**
-* @var Just in case
+* @var Just in case (fake)
 **/
 use pocketmine\item\Item;
 /**
@@ -44,10 +44,13 @@ $player->getInventory()->clearAll();
 public function InteractionItem(PlayerInteractEvent $event){
 $player = $event->getPlayer();
 $item = $event->getItem();
-if($item->getCustomName() == ""){
-  
-}else if($item->getCustomName() == ""){
-}else if($item->getCustomName() == ""){
-}
+if($item->getCustomName() == "§1§kA§r§bTravel§1§kA"){
+FormsManager::getGames($player);
+}else if($item->getCustomName() == "§b§kB§r§aCosmetics§b§kB"){
+FormsManager::getCosmetic($player);
+}else if($item->getCustomName() == "§e§kC§e§fInformation§e§kC"){
+FormsManager::getInfo($player);
+      }
+   }
 }
 ?>
