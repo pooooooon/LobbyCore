08d0b3fa1528a9209e6fe8b90238d4f60f40028f
SrClauYT <69334014+SrClau@users.noreply.github.com>
2020-10-14 00:29:14 -0300

    JoinEvent.php

--- src/CF/Events/JoinEvent.php
+++ src/CF/Events/JoinEvent.php
@@ -20,7 +20,34 @@ class EventJoin implements LT {
 
 public function onJoinEvent(PlayerJoinEvent $event){
 $player = $event->getPlayer();
+$event->setJoinMessage("");
+$player->setHealth(20);
+$player->setFood(20);
+$player->removeAllEffects();
+$player->setScale(1);
+$player->getArmorInventory()->clearAll();
+$player->getInventory()->clearAll();
+//$player->teleport();
 }
 
+public function onRespawn(PlayerRespawnEvent $event){
+$player = $event->getPlayer();
+//$event->setRespawnPosition();
+$player->setHealth(20);
+$player->setFood(20);
+$player->removeAllEffects();
+$player->setScale(1);
+$player->getArmorInventory()->clearAll();
+$player->getInventory()->clearAll();
+}
+
+public function InteractionItem(PlayerInteractEvent $event){
+$player = $event->getPlayer();
+$item = $event->getItem();
+if($item->getCustomName() == ""){
+  
+}else if($item->getCustomName() == ""){
+}else if($item->getCustomName() == ""){
+}
 }
 ?>
