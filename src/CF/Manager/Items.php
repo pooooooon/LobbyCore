9ee452134d6da629d86865576bc8e1c2317ecc56
SrClauYT <69334014+SrClau@users.noreply.github.com>
2020-10-14 14:12:27 -0300

    Update Items.php

--- src/CF/Manager/Items.php
+++ src/CF/Manager/Items.php
@@ -1,1 +1,27 @@
+<?php
 
+namespace CF;
+
+use pocketmine\utils\TextFormat as Text;
+use pocketmine\item\Item;
+use pocketmine\scheduler\Task;
+use pocketmine\utils\Config;
+use pocketmine\level\Position;
+use pocketmine\{Player, Server};
+use pocketmine\network\mcpe\protocol\LevelEventPacket;
+use pocketmine\network\mcpe\protocol\LevelSoundEventPacket;
+use CF\Main;
+
+class Items {
+
+private $plugin;
+
+public function __construct(Main $plugin){
+$this->plugin = $plugin
+}
+
+public function getItems(Player $player){
+
+
+}
+?>
