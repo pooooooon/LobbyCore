5c1f3bd0eca77d402ffe5d63364ae202ed8d5f14
SrClauYT <69334014+SrClau@users.noreply.github.com>
2020-10-14 14:14:21 -0300

    Update Items.php

--- src/CF/Manager/Items.php
+++ src/CF/Manager/Items.php
@@ -21,6 +21,11 @@ $this->plugin = $plugin
 
 public function getItems(Player $player){
 
+$inventory = $player->getInventory();
+$inventory->setItem(5, Item::get(345, 0, 1)->setCustomName("§1§kA§r§bTravel§1§kA"));
+$inventory->setItem(0, Item::get(234, 0, 1)->setCustomName("§b§kB§r§aCosmetics§b§kB"));
+$inventory->setItem(8, Item::get(450, 0, 1)->setCustomName("§e§kC§e§fInformation§e§kC"));
+}
 
 }
 ?>
