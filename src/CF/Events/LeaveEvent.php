246af34ef4269519cd7acb3fe1172d37a8b90b33
SrClauYT <69334014+SrClau@users.noreply.github.com>
2020-10-13 22:43:01 -0300

    LeaveEvent.php

--- src/CF/Events/LeaveEvent.php
+++ src/CF/Events/LeaveEvent.php
@@ -1,1 +1,17 @@
+<?php
 
+namespace CF\Events;
+
+use pocketmine\event\Listener as LT;
+use pocketmine\{Player, Server};
+use pocketmine\utils\{Config, TextFormat as CF};
+use pocketmine\event\player\{PlayerQuitEvent};
+
+class LeaveEvent implements Listener {
+  
+public function EventLeave(PlayerQuitEvent $event){
+$player = $event->getPlayer();
+$nombre = $olayer->getName();
+$event->setQuitMessage("");
+}
+}
