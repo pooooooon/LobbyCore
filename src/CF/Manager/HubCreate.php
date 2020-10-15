73002b99c44e64688cd8ec418efa241c7d32ec5f
SrClauYT <69334014+SrClau@users.noreply.github.com>
2020-10-15 12:22:08 -0300

    ..

--- src/CF/Manager/HubCreate.php
+++ src/CF/Manager/HubCreate.php
@@ -5,11 +5,12 @@ namespace CF\Manager;
 use pocketmine\level\{Level, Position};
 use pocketmine\{Player, Server};
 use pocketmine\utils\Config;
-
+use CF\Main;
 class HubCreate {
 
 public static function createHub(Player $player, string $mundo){
-  
+$hub = new Config(Main::getCore()->getDataFolder() . "hub.yml", Config::YAML);
+
 }
 
 public static function createHubSpawn(Player $player){
