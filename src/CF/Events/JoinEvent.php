f02feb18aae7c0abe287a6356bedbd6fb69b6fe9
SrClauYT <69334014+SrClau@users.noreply.github.com>
2020-10-15 11:59:52 -0300

    Update JoinEvent.php

--- src/CF/Events/JoinEvent.php
+++ src/CF/Events/JoinEvent.php
@@ -4,6 +4,7 @@ namespace CF\Events;
 
 use pocketmine\event\Listener as LT;
 use pocketmine\event\player\{PlayerJoinEvent, PlayerRespawnEvent, PlayerInteractEvent};
+use pocketmine\{Player, Server};
 use pocketmine\utils\TextFormat as Text;
 /**
 * @var Just in case (fake)
@@ -14,14 +15,17 @@ use pocketmine\item\Item;
 **/
 use pocketmine\level\{Position, Level};
 use CF\Main;
-use CF\Forms\FormsManager;
+use CF\Forms\{FormsManager};
 class JoinEvent implements LT {
 
 public function onJoinEvent(PlayerJoinEvent $event){
 $player = $event->getPlayer();
+$config = Main::Config();
+$name = $player->getName();
 $event->setJoinMessage("");
 Main::Items()->getItems($player);
 //$player->teleport();
+Server::getInstance()->broadcastMessage($config->get("prefix-myserver") . " §f" . $name . " " . $config->get("join-msg"));
 }
 
 public function onRespawn(PlayerRespawnEvent $event){
@@ -36,8 +40,9 @@ $item = $event->getItem();
 if($item->getCustomName() == "§1§kA§r§bTravel§1§kA"){
 FormsManager::getGames($player);
 }else if($item->getCustomName() == "§b§kB§r§aCosmetics§b§kB"){
+$event->setCancelled();
 FormsManager::getCosmetic($player);
-}else if($item->getCustomName() == "§e§kC§e§fInformation§e§kC"){
+}else if($item->getCustomName() == "§e§kC§r§e§fInformation§e§kC"){
 FormsManager::getInfo($player);
       }
    }
