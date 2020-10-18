c786765a62ee89e5ff79b9d2379c2b8016b9138d
SrClauYT <69334014+SrClau@users.noreply.github.com>
2020-10-18 12:11:05 -0300

    Updated Hub.php

--- src/CF/Commands/Hub.php
+++ src/CF/Commands/Hub.php
@@ -1,33 +1,27 @@
 <?php
 
-namespace CF\Comandos;
+namespace CF\Commands;
 
 use pocketmine\command\{Command, CommandSender, PluginCommand};
 use pocketmine\utils\{Config, TextFormat as Text};
 use pocketmine\{Server, Player};
 use CF\Main;
+use CF\Manager\HubCreate;
 class Hub extends PluginCommand {
 
 public $plugin;
 
 public function __construct(Main $plugin){
-parent::_construct("hub", $plugin);
+parent::__construct("hub", $plugin);
 $this->setAliases(["h", "lobby"]);
 $this->setDescription("§7Return to Hub");
 }
 
-public function execute(CommandSender $player, Command $cmd, string $label, array $args){
-if(isset($args)){
-$player->sendMessage("§8[§eCF§8] §cUsage: §7/hub");
-return false;
-}
+public function execute(CommandSender $player, string $label, array $args){
+Main::Items()->getItems($player);
 $player->setFlying(false);
 $player->setAllowFlight(false);
-$player->setHealth(20);
-$player->setFood(20);
-$player->setScale(1);
-$player->getArmorInventory()->clearAll();
-
+$player->teleport(HubCreate::TeleportHub());
 return true;
 }
 }
