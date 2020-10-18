02f783c9f4def7fe679a054549e53ab91c0fba43
SrClauYT <69334014+SrClau@users.noreply.github.com>
2020-10-18 02:51:10 -0300

    New HubCreate.php

--- src/CF/Manager/HubCreate.php
+++ src/CF/Manager/HubCreate.php
@@ -9,13 +9,20 @@ use CF\Main;
 class HubCreate {
 
 public static function createHub(Player $player, string $mundo){
-$hub = new Config(Main::getCore()->getDataFolder() . "hub.yml", Config::YAML);
+$hub = new Config(Main::getCore()->getDataFolder()."hub.yml", Config::YAML);
 $hub->set("Level", $mundo);
+$hub->save();
+Main::getCore()->getLogger()->notice("Registered World :D");
+}
+
+public static function createSpawn(Player $player){
+$hub = new Config(Main::getCore()->getDataFolder()."hub.yml", Config::YAML);
 $hub->set("Spawns", [
 $player->getFloorX(),
 $player->getFloorY(),
 $player->getFloorZ()
 ]);
+$hub->save();
+Main::getCore()->getLogger()->notice("Registered Spawns :D");
 }
-
 }
