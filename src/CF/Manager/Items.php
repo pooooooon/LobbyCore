d8ea3a64604f146d232ef7b9761c25e1712c8d1b
SrClauYT <69334014+SrClau@users.noreply.github.com>
2020-10-18 12:34:37 -0300

    Update Items.php

--- src/CF/Manager/Items.php
+++ src/CF/Manager/Items.php
@@ -23,7 +23,7 @@ $player->getInventory()->clearAll();
 $inventory = $player->getInventory();
 $inventory->setItem(4, Item::get(345, 0, 1)->setCustomName("§1§kA§r§bTravel§1§kA"));
 $inventory->setItem(0, Item::get(234, 0, 1)->setCustomName("§b§kB§r§aCosmetics§b§kB"));
-$inventory->setItem(8, Item::get(450, 0, 1)->setCustomName("§e§kC§e§fInformation§e§kC"));
+$inventory->setItem(8, Item::get(450, 0, 1)->setCustomName("§e§kC§r§fInformation§e§kC"));
 }
 }
 ?>
